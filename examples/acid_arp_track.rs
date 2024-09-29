7b1c3be6f78af7a9a4d1edd3d597ddc20bd905ea
julien <julien@eudine.fr>
2024-09-29 17:56:10 +0200

    acid_arp example added

---
+++ examples/acid_arp_track.rs
@@ -0,0 +1,51 @@
+use mseq::{Conductor, DeteTrack};
+
+struct MyConductor {
+    acid: DeteTrack,
+    arp: DeteTrack,
+}
+
+impl Conductor for MyConductor {
+    fn init(&mut self, context: &mut mseq::Context<impl mseq::MidiConnection>) {
+        // The sequencer is on pause by default
+        context.start();
+    }
+
+    fn update(&mut self, context: &mut mseq::Context<impl mseq::MidiConnection>) {
+        // First play acid on channel 0 and then arp on channel 1
+        if context.get_step() < 430 {
+            context.midi.play_track(&mut self.acid);
+        } else {
+            context.midi.play_track(&mut self.arp);
+        }
+
+        // Quit after 960 steps
+        if context.get_step() == 959 {
+            context.quit();
+        }
+    }
+}
+
+fn main() {
+    env_logger::init();
+
+    let acid =
+        DeteTrack::load_acid_from_file("examples/res/acid_0.csv", mseq::Note::A, 0, "my_acid")
+            .unwrap();
+    let arp = DeteTrack::load_arp_from_file(
+        "examples/res/arp_0.csv",
+        mseq::ArpDiv::T8,
+        mseq::Note::A,
+        1,
+        "my_arp",
+    )
+    .unwrap();
+
+    if let Err(e) = mseq::run(
+        MyConductor { acid, arp },
+        // The midi port will be selected at runtime by the user
+        None,
+    ) {
+        println!("An error occured: {:?}", e);
+    }
+}
